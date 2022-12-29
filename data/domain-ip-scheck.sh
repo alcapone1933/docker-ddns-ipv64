58cc6106a8d854e506b2557cd4b5db1542c6d01e
alcapone1933 <alcapone1933@cosanostra-cloud.de>
2022-12-29 13:16:40 +0100

    update

--- data/domain-ip-scheck.sh
+++ data/domain-ip-scheck.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+if ! curl -sSL --user-agent "${CURL_USER_AGENT}" --fail "https://ipv64.net" 2>&1 > /dev/null; then
+    echo "$DATUM  FEHLER !!!  - 404 Sie haben kein Netzwerk oder Internetzugang oder die Webseite ipv64.net ist nicht erreichbar"
+    exit 1
+fi
+
 if [[ "${DOMAIN_PRAEFIX_YES}" =~ (YES|yes|Yes) ]] ; then
     for DOMAIN in $(echo "${DOMAIN_IPV64//,/ }"); do
         echo "`date +%Y-%m-%d\ %H:%M:%S`  IP CHECK    - Deine DOMAIN ${DOMAIN} HAT DIE IP=`dig +short ${DOMAIN} A @ns1.ipv64.net`" >> /var/log/cron.log 2>&1
--- data/healthcheck.sh
+++ data/healthcheck.sh
@@ -3,5 +3,5 @@ DATUM=$(date +%Y-%m-%d\ %H:%M:%S)
 set -e
 if ! curl -sSL --user-agent "${CURL_USER_AGENT}" --fail "https://ipv64.net" 2>&1 > /dev/null; then
     echo "$DATUM  FEHLER !!!  - 404 Sie haben kein Netzwerk oder Internetzugang oder die Webseite ipv64.net ist nicht erreichbar"
-    exit 0
+    exit 1
 fi
